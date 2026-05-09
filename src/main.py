2ce009db1aa922a90fe341e647813f12f2a7afd7
vboxuser <vboxuser@ubuntu3.myguest.virtualbox.org>
2026-05-09 10:52:58 +0300

    update process message

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from validator import validate_event
 
 def process_event(event_name):
+    print("Processing started")
     event_name = event_name.strip()
 
     if not validate_event(event_name):
