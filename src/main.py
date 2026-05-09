6a0ef82e26a1204f095f9b70fd5b8cf50fc99881
vboxuser <vboxuser@ubuntu3.myguest.virtualbox.org>
2026-05-09 10:27:40 +0300

    fix bug

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from validator import validate_event
 
 def process_event(event_name):
+    event_name = event_name.strip()
+
     if not validate_event(event_name):
         return "Invalid event"
 
